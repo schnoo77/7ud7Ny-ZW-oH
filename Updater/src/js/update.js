8dc489d3383a78b5ca7bb3a3d4a4bd99a6a749d6
WeslleyMouraDev <weslleymouradev@gmail.com>
2023-10-22 19:31:42 -0300

    feat: atualiza updater

--- Updater/src/js/update.js
+++ Updater/src/js/update.js
@@ -17,7 +17,11 @@ const updateOptions = {
     gitUsername: "schnoo77",  // [Required] Your GitHub Username.
 
     appName: "Mod", //[Required] The Name of the app archive and the app folder.
-    appExecutableName: "run.bat", //[Required] The Executable of the Application to be Run after updating.
+    appExecutableName: "Run.exe", //[Required] The Executable of the Application to be Run after updating.
+
+    appDirectory: "./FruitCash - Mod", //{Default is "Application Data/AppName"} [Optional]  Where the app will receide, make sure your app has permissions to be there.
+    versionFile: "./settings/version.json", // {Default is "./settings/version.json"} [Optional] The Path to the Local Version File.
+    tempDirectory: "./tmp", // {Default is "./tmp"} [Optional] Where the Update archive will download to.
 
     progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
     label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
